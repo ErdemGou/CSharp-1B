42ff203be0aa53e941c1a9abb8f777254035c8b8
Erdem KÖSTEK <84468790+ErdemGou@users.noreply.github.com>
2023-11-19 15:49:39 +0300

    12.Oturum Odevi Kamp 9-10-11 Video / Pair 9 Ortak
    12.Oturum Odevi Kamp 9-10-11 Video / Pair 9 Ortak

---
+++ TobetoCourseAcademy8/WebAPI/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstracts;
+using Entities.Concretes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // Attribute
+    public class CoursesController : ControllerBase
+    {
+        //Loosely coupled / gevşek bağlılık
+        //naming convention
+        //IoC Container -- Inversion of Control / Değişimin kontrolü
+        //Hiçbir katman diğer katmanların somut classlarına bağlı olmamalıdır. Diğer katmanların soyut sınıfları üzerinden işlem yapılmalıdır.
+        ICourseService _courseService;       
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public List<Course> Get()
+        {             
+            //Dependency chain --
+            var result = _courseService.GetAll();
+            return result.Data;
+        }
+    }
+}
